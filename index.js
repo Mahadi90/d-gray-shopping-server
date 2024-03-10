dc4d5e261f85ce23fda5c438b4ac75657fa3f1b3
Mahadi hasan <mahadimhs787@gmail.com>
2024-03-10 23:42:57 +0600

    vercel json added

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    
+    next();
+  }) 
+  
+
 
 
 
